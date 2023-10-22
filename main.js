c9a92d261482757acb72dce5f1a5f749f67bed32
Marya Luiza Rocha <84876774+MaryaLuizaRocha@users.noreply.github.com>
2023-10-22 18:05:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -75,3 +75,29 @@ Quagga.onProcessed(() => {
 window.addEventListener('beforeunload', () => {
     Quagga.stop();
 });
+
+// Adicionar um event listener para o botão "Iniciar Leitura"
+startButton.addEventListener('click', async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    videoElement.srcObject = stream;
+
+    Quagga.init({
+      inputStream: {
+        type: 'LiveStream',
+        constraints: {
+          ...stream.getVideoTracks()[0].getSettings(),
+          facingMode: 'environment',
+        },
+      },
+      decoder: {
+        readers: ['code_128_reader'],
+      },
+    });
+
+    Quagga.start();
+  } catch (error) {
+    console.error('Erro ao acessar a câmera: ', error);
+  }
+});
+
